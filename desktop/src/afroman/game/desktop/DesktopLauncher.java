9daf8a72a8f405930101895bf803c1b7567e8420
Samson <qwertysam@users.noreply.github.com>
2019-05-01 18:02:46 -0400

    now compiles properly
    Also removed IOS version

--- desktop/src/afroman/game/desktop/DesktopLauncher.java
+++ desktop/src/afroman/game/desktop/DesktopLauncher.java
@@ -1,18 +1,18 @@
 package afroman.game.desktop;
 
-import afroman.game.MainGame;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import afroman.game.MainGame;
 
 public class DesktopLauncher {
-    private static final int CAMERA_WIDTH = 240;//1280;
-    private static final int CAMERA_HEIGHT = CAMERA_WIDTH * 9 / 16;//720;
+	private static final int CAMERA_WIDTH = 240;//1280;
+	private static final int CAMERA_HEIGHT = CAMERA_WIDTH * 9 / 16;//720;
 
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Afro Man");
-        // TODO config.setWindowIcon();
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Afro Man");
+		// TODO config.setWindowIcon();
 
-        new Lwjgl3Application(new MainGame(), config);
-    }
+		new Lwjgl3Application(new MainGame(), config);
+	}
 }
